748ac885a7d6ff87845123d6d9b8667abd36bec4
Nogunmesa <nogunmesa@gmail.com>
2023-08-28 20:55:15 -0500

    yoruba learning app

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Yoruba phrases
+lessons = [
+    {"lesson_id": 1, "title": "body parts", "content":["ori", "oju", "eti", "enu", "imu", "irun", "oju", "okan", "owo", "ika", "ese", "atelese"]}
+]
+
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', lessons=lessons)
+
+app.route("/lesson/<int:lesson_id>")
+def lesson(lesson_id):
+    selected_lesson = None
+    for lesson in lessons:
+        if lesson["lesson_id"] == lesson_id:
+            selected_lesson = lesson
+            break
+    return render_template("lesson.html", lesson=selected_lesson)
