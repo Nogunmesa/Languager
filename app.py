d38fd76408a387239907466eef0eb28aa8682d32
Nogunmesa <nogunmesa@gmail.com>
2023-08-28 21:11:02 -0500

    New file

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Sample Yoruba vocabulary and phrases
+# Sample data
+class Lesson:
+    def __init__(self, lesson_id, title, content):
+        self.lesson_id = lesson_id
+        self.title = title
+        self.content = content
+
 lessons = [
-    {"lesson_id": 1, "title": "Colors", "content": ["ọrun", "pupa", "dúdú"]},
-    {"lesson_id": 2, "title": "Animals", "content": ["ẹja", "ẹlẹ́phantí", "àkọ́kọ́"]},
-    # Add more lessons
+    Lesson(1, "Lesson 1", ["Content 1", "Content 2"]),
+    Lesson(2, "Lesson 2", ["Content 3", "Content 4"]),
+    # Add more lessons as needed
 ]
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html", lessons=lessons)
+    return render_template('index.html', lessons=lessons)
 
-@app.route("/lesson/<int:lesson_id>")
+@app.route('/lesson/<int:lesson_id>')
 def lesson(lesson_id):
-    selected_lesson = None
-    for lesson in lessons:
-        if lesson["lesson_id"] == lesson_id:
-            selected_lesson = lesson
-            break
-    return render_template("lesson.html", lesson=selected_lesson)
+    lesson = next((l for l in lessons if l.lesson_id == lesson_id), None)
+    if lesson is None:
+        return "Lesson not found", 404
+    return render_template('lesson.html', lesson=lesson)
 
 if __name__ == "__main__":
     app.run(debug=True)
