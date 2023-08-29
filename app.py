a514051fcd19e8e26f4ebe855b52c057870cfa9f
Nogunmesa <nogunmesa@gmail.com>
2023-08-28 21:22:34 -0500

    work pls

--- app.py
+++ app.py
@@ -3,16 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Sample data
-class Lesson:
-    def __init__(self, lesson_id, title, content):
-        self.lesson_id = lesson_id
-        self.title = title
-        self.content = content
-
 lessons = [
-    Lesson(1, "Lesson 1", ["Content 1", "Content 2"]),
-    Lesson(2, "Lesson 2", ["Content 3", "Content 4"]),
-    # Add more lessons as needed
+    {"lesson_id": 1, "title": "Lesson 1", "content": ["Content 1", "Content 2"]}
 ]
 
 @app.route('/')
@@ -21,7 +13,7 @@ def index():
 
 @app.route('/lesson/<int:lesson_id>')
 def lesson(lesson_id):
-    lesson = next((l for l in lessons if l.lesson_id == lesson_id), None)
+    lesson = next((l for l in lessons if l["lesson_id"] == lesson_id), None)
     if lesson is None:
         return "Lesson not found", 404
     return render_template('lesson.html', lesson=lesson)
