cc8438928a6c0067d359eefd82efe3d3e1d09601
Nogunmesa <nogunmesa@gmail.com>
2023-08-28 21:28:05 -0500

    work pls

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-# Sample data
-lessons = [
-    {"lesson_id": 1, "title": "Lesson 1", "content": ["Content 1", "Content 2"]}
-]
-
 @app.route('/')
-def index():
-    return render_template('index.html', lessons=lessons)
-
-@app.route('/lesson/<int:lesson_id>')
-def lesson(lesson_id):
-    lesson = next((l for l in lessons if l["lesson_id"] == lesson_id), None)
-    if lesson is None:
-        return "Lesson not found", 404
-    return render_template('lesson.html', lesson=lesson)
+def hello():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug=True)
