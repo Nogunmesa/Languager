aedc4e428096bc9ef4a7d2d6e50452191dd9600e
Nogunmesa <nogunmesa@gmail.com>
2023-08-28 21:04:03 -0500

    New file

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request
 
-
 app = Flask(__name__)
 
-# Yoruba phrases
+# Sample Yoruba vocabulary and phrases
 lessons = [
-    {"lesson_id": 1, "title": "body parts", "content":["ori", "oju", "eti", "enu", "imu", "irun", "oju", "okan", "owo", "ika", "ese", "atelese"]}
+    {"lesson_id": 1, "title": "Colors", "content": ["ọrun", "pupa", "dúdú"]},
+    {"lesson_id": 2, "title": "Animals", "content": ["ẹja", "ẹlẹ́phantí", "àkọ́kọ́"]},
+    # Add more lessons
 ]
 
-@app.route('/')
-def hello():
-    return render_template('index.html', lessons=lessons)
+@app.route("/")
+def index():
+    return render_template("index.html", lessons=lessons)
 
-app.route("/lesson/<int:lesson_id>")
+@app.route("/lesson/<int:lesson_id>")
 def lesson(lesson_id):
     selected_lesson = None
     for lesson in lessons:
@@ -20,3 +21,6 @@ def lesson(lesson_id):
             selected_lesson = lesson
             break
     return render_template("lesson.html", lesson=selected_lesson)
+
+if __name__ == "__main__":
+    app.run(debug=True)
